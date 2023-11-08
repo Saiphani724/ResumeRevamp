83e45c1ec8a995b6bc16c2939d14af9cf68da49a
lil-saiphani <saiphani.p@leaninnovationlabs.com>
2023-11-08 13:18:51 -0500

    end-to-end working version - v1

--- backend/common/parser_helpers.py
+++ backend/common/parser_helpers.py
@@ -1,11 +1,13 @@
-def get_parse_resume_prompt(resume_txt):
-    return f"""Please provide me the following in JSON format:
+def get_parse_resume_messages(resume_txt):
+    prompt = f"""Please provide me the following in JSON format:
 
 - "personal_details" which should include: name, location, phone, email, LinkedIn, and GitHub.
 - "education" should be categorized as school/university, duration_years, GPA, courses.
 - "experience" should be categorized as company_name, duration, and summary which should contain any information that has been provided under any role.
 -  Keep the summary as it is, and don't change anything.
-- "projects" should be organized similarly to "experience" where it should be divided title, duration, and summary.Summary should contain any information that has been provided under any project.keep it as it is without changing anything
+- "projects" should be organized similarly to "experience" where it should be divided into title, duration, skills, and summary.
+    Skills should contain all technical skills, languages, frameworks, tools, and databases used in the project.
+    Summary should contain any information that has been provided under any project. keep it as it is without changing anything
 - "technical_skills" has different sections like languages, frameworks, tools, databases.
 - Add "course_work" section containing courses if only the courses are not mentioned in "education"
 - "extra_curriculars," if any, should be sorted into "summary" containing the exact information.
@@ -17,9 +19,16 @@ Do not include any information twice.
 Here is the resume. You will have to parse this into the above-mentioned format:
 {resume_txt}
  """
+    return [
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    ]
 
-def get_job_details_prompt(job_details_txt):
-    return f""" Please provide me the following in JSON format:
+
+def get_job_details_messages(job_details_txt):
+    prompt =  f""" Please provide me the following in JSON format:
         - Job Title: The role that the job details are about
         - Job Description: A description of the job
         - Prerequisites: The minimum skills required for the job. Please include all necessary requirements for the job.
@@ -28,5 +37,11 @@ def get_job_details_prompt(job_details_txt):
         Please make sure not to repeat data in multiple fields.
 
         Here is the text format of the job details.You will have to parse this into the above-mentioned format:
-        {jobdetails_txt}
+        {job_details_txt}
     """
+    return [
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    ]
--- backend/resume_gen.py
+++ backend/resume_gen.py
@@ -1,28 +1,30 @@
-from common.parser_helpers import get_parse_resume_prompt, get_job_details_prompt
+from common.parser_helpers import get_job_details_messages, get_parse_resume_messages
 from common.resume_gen_helpers import get_resume_gen_messages, resume_gen_functions
 from common.latex_resume_gen_helper import get_resume_latex_gen_messages
 from gpt_model import GPTModel
 import json
 
-model = GPTModel("gpt-4-1106-preview")
+model_gpt35 = GPTModel("gpt-3.5-turbo-0613")
+# model_gpt4 = GPTModel("gpt-4-0613")
+model_gpt4 = GPTModel("gpt-4-1106-preview")
 
 
 async def parse_resume(resume):
-    resume_json = {}
+    messages = get_parse_resume_messages(resume)
+    resume_json = await model_gpt35.get_completion(messages)
     return resume_json
 
-
 async def parse_job_details(job_details):
-    job_details_json = {}
+    messages = get_job_details_messages(job_details)
+    job_details_json = await model_gpt35.get_completion(messages)
     return job_details_json
-
 
 async def generate_resume(resume_json, job_details_json):
     resume_new = {}
     messages = get_resume_gen_messages(resume_json, job_details_json)
     functions = resume_gen_functions
     function_call = {"name": "revamp_resume"}
-    resume_new = await model.get_completion_with_function_call(
+    resume_new = await model_gpt4.get_completion_with_function_call(
         messages, functions, function_call
     )
     return resume_new
@@ -30,5 +32,5 @@ async def generate_resume(resume_json, job_details_json):
 
 async def generate_latex_resume(resume_new):
     messages = get_resume_latex_gen_messages(resume_new)
-    resume_latex = await model.get_completion(messages)
+    resume_latex = await model_gpt4.get_completion(messages)
     return resume_latex
