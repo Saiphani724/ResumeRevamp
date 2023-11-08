67dbb7145baa1d7e76ea95b02ff32775f69fb87b
andy6294 <ankitreddy.vanguru@stonybrook.edu>
2023-11-08 00:04:30 -0500

    Model updation

--- backend/common/parser_helpers.py
+++ backend/common/parser_helpers.py
@@ -1,17 +1,22 @@
 def get_parse_resume_prompt(resume_txt):
     return f"""Please provide me the following in JSON format:
-        - "personal_details" which should include: name, location, phone, email, LinkedIn, and GitHub.
-        - "education" should be categorized as school/university, duration_years, GPA, courses.
-        - "experience" should be categorized as company_name, duration, and summary.
-        - Keep the "summary" as it is, and don't change anything.
-        - "projects" should be organized similarly to "experience."
-        - "technical_skills" has different sections like languages, frameworks, tools, databases.
-        - Add "course_work" section containing courses if only the courses are not mentioned in "education"
-        - "extra_curriculars," if any, should be sorted into "summary" containing the exact information.
-        - If any information provided in the resume doesn't fall under one of the above categories,
-        include these sections in "others." Include each section under the name of the section.
-    Do not include any information twice.
-    Here is the resume. You will have to parse this into the above-mentioned format: {resume_txt} """
+
+- "personal_details" which should include: name, location, phone, email, LinkedIn, and GitHub.
+- "education" should be categorized as school/university, duration_years, GPA, courses.
+- "experience" should be categorized as company_name, duration, and summary which should contain any information that has been provided under any role.
+-  Keep the summary as it is, and don't change anything.
+- "projects" should be organized similarly to "experience" where it should be divided title, duration, and summary.Summary should contain any information that has been provided under any project.keep it as it is without changing anything
+- "technical_skills" has different sections like languages, frameworks, tools, databases.
+- Add "course_work" section containing courses if only the courses are not mentioned in "education"
+- "extra_curriculars," if any, should be sorted into "summary" containing the exact information.
+- If any information provided in the resume doesn't fall under one of the above categories,
+  include these sections in "others." Include each section under the name of the section.
+
+Do not include any information twice.
+
+Here is the resume. You will have to parse this into the above-mentioned format:
+{resume_txt}
+ """
 
 def get_job_details_prompt(job_details_txt):
     return f""" Please provide me the following in JSON format:
